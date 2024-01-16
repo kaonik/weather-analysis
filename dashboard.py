99ff7daf5319e3c7ec4035b988f625cb3eb566b2
Samuel Morales <sammorales95@gmail.com>
2024-01-15 22:31:04 -0500

    Added averages per day

--- dashboard.py
+++ dashboard.py
@@ -21,16 +21,22 @@ def fetch_data(query,db_conn_params=db_conn_params):
 
 # Get latitude, longitude, and forecast data with locationid
 query ="""
-    SELECT l.*, f.*
+    SELECT DATE(f.timestampiso) AS day,
+           AVG(f.temperature) AS avg_temperature,
+           AVG(f.humidity) AS avg_humidity,
+           AVG(f.windspeed) AS avg_wind_speed,
+           AVG(f.pressure) AS avg_pressure,
+           l.latitude, l.longitude
     FROM location l
     JOIN forecast f ON l.locationid = f.locationid
+    GROUP BY day, l.latitude, l.longitude
     LIMIT 1000;
-                """
+"""
 
 df = fetch_data(query)
 
 # Create plotly scatter mapbox figure
-fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="temperature", color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=3, hover_data=["timestampiso"])
+fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="avg_temperature", color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=3, hover_data=["day","avg_temperature","avg_humidity","avg_wind_speed","avg_pressure"])
 fig.update_layout(mapbox_style="open-street-map")
 
 # Create dash app
